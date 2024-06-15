4690f4bde7df44dc74ca43c6169c0cd0aa7d13e8
SavvasF <filippides.savvas@gmail.com>
2024-06-15 19:27:23 +0300

    Created some menu scenes

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : Menu
+{
+    public static MainMenu Instance { get; private set; }
+
+    private void Start()
+    {
+        if (Instance)
+        {
+            Debug.LogError("Trying to create more than 1 MainMenu!");
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        Debug.Log("MainMenu Created!");
+    }
+
+}
