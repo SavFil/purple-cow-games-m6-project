9d2100a80943934c9fd295818e7ab8564b6e9ad2
AbigailStoffberg <abizoide@gmail.com>
2024-06-24 12:25:09 +0200

    Added code to update game object position based on player input in FixedUpdate in the craft script.

--- Assets/Scripts/Ganeplay/Craft.cs
+++ Assets/Scripts/Ganeplay/Craft.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class Craft : MonoBehaviour
 {
     CraftData craftData = new CraftData();
+    Vector3 newPosition = new Vector3();
+    private void FixedUpdate()
+    {
+        if (InputManager.Instance)
+        {
+            craftData.positionX += InputManager.Instance.playerState[0].movement.x;
+            craftData.positionY += InputManager.Instance.playerState[0].movement.y;
+            newPosition.x =(int)craftData.positionX;
+            newPosition.y = (int)craftData.positionY;
+            gameObject.transform.position = newPosition;
+        }
+    }
 }
 
 public class CraftData
