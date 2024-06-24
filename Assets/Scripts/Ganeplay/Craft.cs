0c8a5421f80e5ce7aabc9cc51b0412b57d63f463
AbigailStoffberg <abizoide@gmail.com>
2024-06-24 10:44:52 +0200

    Created Gameplay folder and within it the craft script. Edited craft script.

---
+++ Assets/Scripts/Ganeplay/Craft.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Craft : MonoBehaviour
+{
+    CraftData craftData = new CraftData();
+}
+
+public class CraftData
+{
+    public float positionX;
+    public float positionY;
+}
