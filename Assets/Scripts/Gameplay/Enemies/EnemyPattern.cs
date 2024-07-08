684697c37fa5af07e154c4f2a420360c50870a0a
AbigailStoffberg <abizoide@gmail.com>
2024-07-08 22:00:26 +0200

    Created enemy pattern script.

--- Assets/Scripts/Gameplay/Enemies/EnemyPattern.cs
+++ Assets/Scripts/Gameplay/Enemies/EnemyPattern.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class EnemyPattern : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private int UID;
 
-    // Update is called once per frame
-    void Update()
+    [MenuItem("GameObject/SHMUP/EnemyPattern", false, 10)]
+
+    static void CreateEnemyPatternObject(MenuCommand menuCommand)
     {
-        
+        Helpers helper = (Helpers)Resources.Load("Helper");
+        if (helper == null)
+        {
+            GameObject go = new GameObject("EnemyPatter" + helper.nextFreePatternID);
+            EnemyPattern pattern = go.AddComponent<EnemyPattern>();
+            pattern.UID = helper.nextFreePatternID;
+            helper.nextFreePatternID++;
+
+            // Register creation with undo system
+            Undo.RegisterCompleteObjectUndo(go, "Create " + go.name);
+            Selection.activeObject = go;
+        }
+        else Debug.LogError("Could not find Helper");
     }
 }
