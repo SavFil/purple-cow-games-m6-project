c1767dd6ebbe428bab7aaf6fc7a9242db1d93f6f
AbigailStoffberg <abizoide@gmail.com>
2024-07-08 22:40:30 +0200

    Added pattern steps to enemy pattern script.

--- Assets/Scripts/Gameplay/Enemies/EnemyPattern.cs
+++ Assets/Scripts/Gameplay/Enemies/EnemyPattern.cs
@@ -5,22 +5,25 @@ using UnityEngine;
 
 public class EnemyPattern : MonoBehaviour
 {
+    public List<EnemyStep> steps = new List<EnemyStep>();
+
+    public Enemy enemyPrefab;
+
     private int UID;
 
     [MenuItem("GameObject/SHMUP/EnemyPattern", false, 10)]
-
     static void CreateEnemyPatternObject(MenuCommand menuCommand)
     {
         Helpers helper = (Helpers)Resources.Load("Helper");
-        if (helper == null)
+        if (helper!=null)
         {
-            GameObject go = new GameObject("EnemyPatter" + helper.nextFreePatternID);
+            GameObject go = new GameObject("EnemyPattern"+helper.nextFreePatternID);
             EnemyPattern pattern = go.AddComponent<EnemyPattern>();
             pattern.UID = helper.nextFreePatternID;
             helper.nextFreePatternID++;
 
             // Register creation with undo system
-            Undo.RegisterCompleteObjectUndo(go, "Create " + go.name);
+            Undo.RegisterCompleteObjectUndo(go, "Create "+go.name);
             Selection.activeObject = go;
         }
         else Debug.LogError("Could not find Helper");
