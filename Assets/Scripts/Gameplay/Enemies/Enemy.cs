c749142acc064fca734314af0a1433f5011eafce
AbigailStoffberg <abizoide@gmail.com>
2024-07-08 20:45:55 +0200

    Enemy script.

--- Assets/Scripts/Gameplay/Enemies/Enemy.cs
+++ Assets/Scripts/Gameplay/Enemies/Enemy.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public EnemyData data;
 
-    // Update is called once per frame
-    void Update()
+    private EnemyPattern pattern;
+
+    public void SetPattern(EnemyPattern inPattern)
     {
-        
+        pattern = inPattern; 
+        pattern = inPattern; 
     }
+}
+
+[Serializable]
+
+public struct EnemyData
+{
+    public float progressTimer;
+
+    public float positionX;
+    public float positionY;
+
+    public int patternUID;
+
 }
