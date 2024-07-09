31cfe342ca6389a8d29eb42e9684d10e3a865871
AbigailStoffberg <abizoide@gmail.com>
2024-07-09 08:07:51 +0200

    Added CalculatePosition and CalculateRotation to the enemy pattern.

--- Assets/Scripts/Gameplay/Enemies/EnemyPattern.cs
+++ Assets/Scripts/Gameplay/Enemies/EnemyPattern.cs
@@ -7,7 +7,7 @@ public class EnemyPattern : MonoBehaviour
 {
     public List<EnemyStep> steps = new List<EnemyStep>();
 
-    public  Enemy enemyPrefab;
+    public Enemy enemyPrefab;
 
     private Enemy spawnedEnemy;
 
@@ -17,15 +17,15 @@ public class EnemyPattern : MonoBehaviour
     static void CreateEnemyPatternObject(MenuCommand menuCommand)
     {
         Helpers helper = (Helpers)Resources.Load("Helper");
-        if (helper!=null)
+        if (helper != null)
         {
-            GameObject go = new GameObject("EnemyPattern"+helper.nextFreePatternID);
+            GameObject go = new GameObject("EnemyPattern" + helper.nextFreePatternID);
             EnemyPattern pattern = go.AddComponent<EnemyPattern>();
             pattern.UID = helper.nextFreePatternID;
             helper.nextFreePatternID++;
 
             // Register creation with undo system
-            Undo.RegisterCompleteObjectUndo(go, "Create "+go.name);
+            Undo.RegisterCompleteObjectUndo(go, "Create " + go.name);
             Selection.activeObject = go;
         }
         else Debug.LogError("Could not find Helper");
@@ -35,5 +35,15 @@ public class EnemyPattern : MonoBehaviour
     {
         spawnedEnemy = Instantiate(enemyPrefab, transform.position, transform.rotation).GetComponent<Enemy>();
         spawnedEnemy.SetPattern(this);
+    }
+
+    public Vector2 CalculatePosition(float progressTimer)
+    {
+        return Vector2.zero;
+    }
+
+    public Quaternion CalculateRotation(float progressTimer)
+    {
+        return Quaternion.identity;
     }
 }
