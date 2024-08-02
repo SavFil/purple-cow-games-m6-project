46074699354afe12c3eb9b5073c7fd48a7cf24bd
Ilias Tzatzalis <105108350+iltz7981@users.noreply.github.com>
2024-08-02 19:49:01 +0300

    Background / Hit Particles / Enemy Bullets Setup

--- Assets/Scripts/Effects/AnimatedChar.cs
+++ Assets/Scripts/Effects/AnimatedChar.cs
@@ -6,8 +6,20 @@ using UnityEngine;
 
 public class AnimatedChar : MonoBehaviour
 {
+
+    public enum TypeOfAnimation
+    {
+        Number, Hit, Bullet
+    }
+    [Header("Type Of Animation")]
+    public TypeOfAnimation typeOfAnimation;
+    public bool notHavingLoop;
+    bool loopEnded;
+
+    [Header("Animation Settings")]
     public Sprite[] charSprites;
     public SpriteRenderer spriteRenderer;
+
 
     public int digit = 0;
     private int frame = 0;
@@ -16,9 +28,10 @@ public class AnimatedChar : MonoBehaviour
 
     public int noOfCharacters;
     public int noOfFrames;
-    
+
     public float FPS = 10f;
     private float timer;
+
 
     void Start()
     {
@@ -35,19 +48,51 @@ public class AnimatedChar : MonoBehaviour
         spriteRenderer.sprite = charSprites[spriteIndex];
     }
 
-    void Update()
+    public virtual void Update()
     {
+        if (notHavingLoop && loopEnded) return;
         timer -= Time.deltaTime;
 
         if (timer <= 0f)
         {
             timer = 1f / FPS;
             frame++;
-            if (frame>=noOfFrames)
+            
+            if (frame >= noOfFrames)
             {
                 frame = 0;
+                if (notHavingLoop)
+                {
+                    loopEnded = true;
+                    EndAnimation();
+                }
             }
             UpdateSprite(frame);
         }
     }
+
+    public void EndAnimation()
+    {
+        switch (typeOfAnimation)
+        {
+            case TypeOfAnimation.Number:
+                break;
+            case TypeOfAnimation.Hit:
+                Destroy(gameObject);
+                break;
+            case TypeOfAnimation.Bullet:
+                //need the bullet hit for the craft when the health system is implemented
+                //trial with no hit
+                Bullet bullet = gameObject.GetComponent<Bullet>();
+                if (bullet != null)
+                {
+                    GameManager.Instance.bulletManager.DeActivateBullet(bullet.index);
+                    loopEnded = false;
+                }
+                break;
+        }
+    }
+
+
+
 }
