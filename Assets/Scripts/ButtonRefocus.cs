79635542b535f378d31a0e90df8c4337726dbe64
SavvasF <filippides.savvas@gmail.com>
2024-08-19 13:04:21 +0300

    images and items #2

---
+++ Assets/Scripts/ButtonRefocus.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+// If there is no selected item, set the selected item to the event system's first selected item
+public class ButtonRefocus : MonoBehaviour
+{
+    GameObject lastselect;
+
+    void Start()
+    {
+        lastselect = gameObject;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EventSystem.current == null) return;//!gia ControlsOptionsMenu
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(lastselect);
+        }s
+        else
+        {
+            lastselect = EventSystem.current.currentSelectedGameObject;
+        }
+    }
+
+}
